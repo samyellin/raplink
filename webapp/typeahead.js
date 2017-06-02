c5d72db1defd7e0782f19be6fbb4d401fa62b2f2
samyellin <samyellin@Sams-MacBook-Pro-2.local>
2017-06-02 14:33:15 -0400

    Use https

--- webapp/typeahead.js
+++ webapp/typeahead.js
@@ -1,7 +1,7 @@
 $(function() {
   $( "#start" ).autocomplete({
     source: function( request, response ) {
-      var typeaheadUrl = 'http://flask-env.unrhcrvngy.us-west-2.elasticbeanstalk.com/raplink/api/v1.0/typeahead?artist=' + encodeURI($("#start").val());
+      var typeaheadUrl = 'https://flask-env.unrhcrvngy.us-west-2.elasticbeanstalk.com/raplink/api/v1.0/typeahead?artist=' + encodeURI($("#start").val());
       $.ajax({
         url: typeaheadUrl,
         success: function( data ) {
@@ -17,7 +17,7 @@ $(function() {
 
   $( "#end" ).autocomplete({
     source: function( request, response ) {
-      var typeaheadUrl = 'http://flask-env.unrhcrvngy.us-west-2.elasticbeanstalk.com/raplink/api/v1.0/typeahead?artist=' + encodeURI($("#end").val());
+      var typeaheadUrl = 'https://flask-env.unrhcrvngy.us-west-2.elasticbeanstalk.com/raplink/api/v1.0/typeahead?artist=' + encodeURI($("#end").val());
       $.ajax({
         url: typeaheadUrl,
         success: function( data ) {
